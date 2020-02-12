132904476315c2a0675b32f18ca953377e34d6c0
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-12 16:49:35 +0900

    이정주의 프로젝트 내 프로젝트에 연결

---
+++ LJJMyIntroduce/src/zzzzz.java
@@ -0,0 +1,14 @@
+
+public class zzzzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("자신의 이름 : 이정주.");
+		System.out.println("나이 : 26.");
+		System.out.println("짝에게 하고 싶은말 : 잘 부탁드립니다!!!");
+		
+		
+	}
+
+}
