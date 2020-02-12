a8a6897345d3e441652e61910bb0b63131eb2112
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-12 15:25:25 +0900

    깃허브 연동 테스트

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,21 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 한 줄 주석
+		// 글은 존재할 수 없다
+		
+		/*
+		이것은
+		두줄 이상 아니지
+		한줄 이상의 주석이다
+		범위주석
+		*/
+		
+		System.out.print("hello java");
+
+	}
+
+}
