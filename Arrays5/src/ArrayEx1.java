526511f3cb2b79346f0b209eb8d87991f9a10f3f
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-17 10:30:22 +0900

    배열 시작

---
+++ Arrays5/src/ArrayEx1.java
@@ -0,0 +1,35 @@
+
+public class ArrayEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int score1 = 0;
+		int score2 = 0;
+		int score3 = 0;
+		int score4 = 0;
+		int score5 = 0;
+	
+		System.out.println(score1);
+		System.out.println(score2);
+		System.out.println(score3);
+		System.out.println(score4);
+		System.out.println(score5);
+		
+		System.out.println("=====배열 시작=====");
+		System.out.println();
+//		배열(Array)
+//		표현식 
+//		타입[] 변수이름;
+		int[] scoreArr = null;
+		scoreArr = new int[5];
+		
+		System.out.println(scoreArr[0]);
+		System.out.println(scoreArr[1]);
+		System.out.println(scoreArr[2]);
+		System.out.println(scoreArr[3]);
+		System.out.println(scoreArr[4]);
+		
+	}
+
+}
