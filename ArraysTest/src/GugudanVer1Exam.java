27a5a400f852d07748fdeb20fc68cc4da234bcc2
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-18 14:37:59 +0900

    배열 구구단 과제

---
+++ ArraysTest/src/GugudanVer1Exam.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @author TJ
+ * 구구단 만들기
+ * 1차원 배열
+ * 결과만 1차원 배열에 넣는다
+ * 그걸 출력한다.
+ * 
+ */
+public class GugudanVer1Exam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] mulNumArr = new int[0];
+		
+//		2 + "*" + = (2 * i)
+//		2 * 1 = mulNumArr[0] 3 * 1 = mulNumArr[1]  ,,, 	9 * 1 = mulNumArr[8]
+//		2 * 2 = 4 3 * 2 = 6  ,,, 	9 * 2 = 18
+//		2 * 3 = 6 3 * 3 = 9  ,,, 	9 * 3 = 27
+//		
+//		
+//		2 * 9 = 18 3 * 9 = 27  ,,, 	9 * 9 = mulNumArr[??]
+
+	}
+
+}
---
+++ ArraysTest/src/GugudanVer2Exam.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author TJ
+ * 구구단 만들기
+ * 2차원 배열
+ * 결과만 2차원 배열에 넣는다
+ * 그걸 출력한다.
+ * 
+ */
+public class GugudanVer2Exam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] mulNumArr = new int[0][0];
+		
+//		2 * 1 = mulNumArr[0][0] 2 * 2 = 4
+//		3 * 1 = 3 3 * 2 = 6  ,,,,,,,   3 * 9 = 27		
+//		,
+//		,
+//		,
+//		9 * 1 = 9 9 * 2 = 18 ,,,,,,    9 * 9 = mulNumArr[?][?]
+
+	}
+
+}
